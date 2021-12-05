d31440496bcc910010061e066519249634864bc7
chandrapavan <chandrapavan507@gmail.com>
2021-12-04 23:21:07 -0500

    API URL point to existing API

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://localhost:44392/breweries";
-//export const API_URL = "https://api.openbrewerydb.org/breweries";
+//export const API_URL = "https://localhost:44392/breweries";
+export const API_URL = "https://api.openbrewerydb.org/breweries";
